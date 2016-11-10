5ab28d6530bb8542cd1f0480dcfd2c2ff2c87baf
Martin Matějka <martinmatejka@MacBook-Pro.local>
2016-11-11 00:28:55 +0100

    first commit

---
+++ Drawing/Canvas.swift
@@ -0,0 +1,32 @@
+//
+//  Canvas.swift
+//  Drawing
+//
+//  Created by Martin Matějka on 24.08.16.
+//  Copyright © 2016 Martin Matějka. All rights reserved.
+//
+
+import UIKit
+
+class Canvas: UIView {
+    
+    
+    
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+   /* override func drawRect(rect: CGRect) {
+        
+        let context = UIGraphicsGetCurrentContext()
+        CGContextSetLineWidth(context, 4.0)
+        CGContextMoveToPoint(context, 10.0, 10.0)
+        //CGContextSetFillColorWithColor(context, UIColor.blueColor().CGColor)
+        CGContextSetStrokeColorWithColor(context, UIColor.blueColor().CGColor)
+        let pointForRect: CGPoint = CGPoint(x: 50, y: 50)
+        CGContextAddRect(context, CGRect(origin: pointForRect, size: CGSize(width: 40, height: 40)))
+                //CGContextAddLineToPoint(context, 20.0, 180.0)
+        CGContextStrokePath(context)
+        
+    }
+    */
+    
+}
---
+++ Drawing/CanvasVC.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Drawing
+//
+//  Created by Martin Matějka on 24.08.16.
+//  Copyright © 2016 Martin Matějka. All rights reserved.
+//
+
+import UIKit
+
+class CanvasVC: UIViewController {
+
+    
+    @IBOutlet weak var colorControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func changeColor(_ sender: UISegmentedControl) {
+        let drawingColorSelection =
+            DrawingColor(rawValue: UInt(sender.selectedSegmentIndex))
+        if let drawingColor = drawingColorSelection {
+            let funView = view as! QuartzFun;
+            switch drawingColor {
+            case .red:
+                funView.currentColor = UIColor.red
+                funView.useRandomColor = false
+            case .blue:
+                funView.currentColor = UIColor.blue
+                funView.useRandomColor = false
+            case .yellow:
+                funView.currentColor = UIColor.yellow
+                funView.useRandomColor = false
+            case .green:
+                funView.currentColor = UIColor.green
+                funView.useRandomColor = false
+            case .random:
+                funView.useRandomColor = true
+            } }
+    }
+
+    @IBAction func changeShape(_ sender: UISegmentedControl) {
+        let shapeSelection = Shape(rawValue: UInt(sender.selectedSegmentIndex))
+        if let shape = shapeSelection {
+            let funView = view as! QuartzFun;
+            funView.shape = shape;
+            colorControl.isHidden = shape == Shape.image
+        }
+    }
+
+}
+
